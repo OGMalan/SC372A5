92bcf265e5946811485a6dbc2f271af5d3ca7370
OGMalan <27867110+OGMalan@users.noreply.github.com>
2018-09-18 01:25:21 +0200

    Revamped code in an attempt to get it to work

--- Romberg.py
+++ Romberg.py
@@ -11,41 +11,44 @@ def Trap(f,a,b,n):
     #print 'Integral of step '+str(n)+' is: '+ str(h*s)
     return h*s
 
-def Richardson(f,a,b,tol):
-    '''Iteratively applies Richardson extrapolation to increase the accuracy of the estimate of the integral.'''
-    j = 1
-    k = 1
-    Rj = Trap(f,a,b,j)
-    running = True
-    while running:
-        j = k
-        k += 1
-        Rk = Trap(f,a,b,k)
-        Rj = (4 **(k - j) * Rk - Rj) / (4 ** (k - j) - 1)
-        E = abs((Rk - Rj))
-        if E < tol:
-            running = False
-        Rj = Rk
-        #print 'Error is '+str(E)
-    return Rk,j
+def Richard(k,R,Rk):
+	outlist = []
+	for j in range(k-1,0,-1):
+		if j + 1 == k:
+			output = (4 ** (k - j) * Rk - R[j-1]) / (4 ** (k - j) - 1)
+		else:
+			output = (4 ** (k - j) * R[j] - R[j-1]) / (4 ** (k - j) - 1)
+		outlist.insert(0,output)
+	return outlist
 
-def romberg(f, a, b, tol):
+def romberg(f, a, b, tol=1.0e-6):
     '''I, n_panels = romberg(f, a, b, tol=1.0e-6)
     Return the integral from a to b of f(x), by Romberg integration,
     as well as the number of panels used'''
-    return Richardson(f,a,b,tol)
+    k = 1
+    R = [Trap(f, a, b, 1), Trap(f, a, b, 2)]
+    running = True
+    while running:
+    	k += 1
+    	Rk = R[0]
+    	R = Richard(k,R,Rk)
+    	E = abs(Rk - R[0])
+    	print 'The error is '+str(E)
+    	if E < tol:
+    		running = False
+    return R[0],k
 
 def f(x):
-    return e**x
+	return 3*x**2 - 4*x**1 + 5
 
 start = time.clock()
-my_rom = romberg(f,1,2,1.0e-6)[0]
-print 'My romberg integral is: ' + str(my_rom)
-print 'Took ' + str(time.clock() - start) + ' seconds'
+my_rom = romberg(f,0,1)
+print 'My romberg integral is: ' + str(my_rom[0])
+print 'Took ' + str(time.clock() - start) + ' seconds and '+str(my_rom[1])+' iterations'
 start = time.clock()
-scipy_rom = rb(f,1,2,tol=1.0e-6)
+scipy_rom = rb(f,0,1,tol=1.0e-6)
 print 'Scipy romberg integral is: ' + str(scipy_rom)
 print 'Took ' + str(time.clock() - start) + ' seconds'
-diff = abs(my_rom - scipy_rom)
+diff = abs(my_rom[0] - scipy_rom)
 tol = 1*10**-6
 print 'The difference is ' + str(diff)
