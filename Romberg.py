a1fa9d4d22111c604cd1b61862bb7f070c83cfbf
OGMalan <27867110+OGMalan@users.noreply.github.com>
2018-09-17 16:01:40 +0200

    modified code

---
+++ Romberg.py
@@ -0,0 +1,36 @@
+import math,scipy
+
+def Trap(f,a,b,n):
+    '''Returns the integral of a function for the range a and b estimated by means of the trapezoidal method using n steps.'''
+    h = (b - a) / float(n)
+    s = 0.5 * (f(a) + f(b))
+    for i in range(1,n):
+        s = s + f(a + i*h)
+    return h*s
+
+def Richardson(f,a,b,tol):
+    '''Returns the integral of a function for the range a,b '''
+    n = 1
+    Rj = Trap(f,a,b,n)
+    running = True
+    while running:
+        n += 1
+        Rk = Trap(f,a,b,n)
+        E = Rj - Rk
+        if E < tol:
+            running = False
+        Rj = Rk
+        return Rk,n
+
+def romberg(f, a, b, tol=1.0e-6):
+    '''I, n_panels = romberg(f, a, b, tol=1.0e-6)
+    Return the integral from a to b of f(x), by Romberg integration,
+    as well as the number of panels used'''
+    return Richardson(f,a,b,tol)
+
+def f(x):
+    return math.sin(x)
+
+
+print romberg(f,1,10,tol=1.0**-6)
+print scipy.integrate,quad(f(x),1,10)
