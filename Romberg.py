a9e70d9092ba680fb74c36454c4ed3c0cfcf88cc
OGMalan <27867110+OGMalan@users.noreply.github.com>
2018-09-17 18:25:15 +0200

    Reverted 3 steps

--- Romberg.py
+++ Romberg.py
@@ -1,4 +1,5 @@
 import math,scipy
+from scipy.integrate import romberg as rb
 
 def Trap(f,a,b,n):
     '''Returns the integral of a function for the range a and b estimated by means of the trapezoidal method using n steps.'''
@@ -16,7 +17,7 @@ def Richardson(f,a,b,tol):
     while running:
         n += 1
         Rk = Trap(f,a,b,n)
-        E = Rj - Rk
+        E = abs(Rj - Rk)
         if E < tol:
             running = False
         Rj = Rk
@@ -31,6 +32,5 @@ def romberg(f, a, b, tol=1.0e-6):
 def f(x):
     return math.sin(x)
 
-
-print romberg(f,1,10,tol=1.0**-6)
-print scipy.integrate,quad(f(x),1,10)
+print 'Romberg value is: ' + str(romberg(f,1,10,tol=1e-6))
+print 'Scipy value is: ' + str(rb(f,1,10,tol=1e-6))
