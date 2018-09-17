15112a8c380df7d6e7b346db85fdbbc8a764afe7
OGMalan <27867110+OGMalan@users.noreply.github.com>
2018-09-17 18:22:31 +0200

    Revert "Tweaked Error Formula"
    This reverts commit 6608bb21b3682ab08526041a77bfc3ce94b82c00.

--- Romberg.py
+++ Romberg.py
@@ -1,6 +1,4 @@
-import math
-from math import e
-from scipy.integrate import romberg as rb
+import math,scipy
 
 def Trap(f,a,b,n):
     '''Returns the integral of a function for the range a and b estimated by means of the trapezoidal method using n steps.'''
@@ -8,7 +6,6 @@ def Trap(f,a,b,n):
     s = 0.5 * (f(a) + f(b))
     for i in range(1,n):
         s = s + f(a + i*h)
-    print 'Integral of step '+str(n)+' is: '+ str(h*s)
     return h*s
 
 def Richardson(f,a,b,tol):
@@ -17,15 +14,13 @@ def Richardson(f,a,b,tol):
     Rj = Trap(f,a,b,n)
     running = True
     while running:
-        j = n
         n += 1
         Rk = Trap(f,a,b,n)
-        E = abs((Rk - Rj) / ((Rk / Rj)**2 - 1))
+        E = Rj - Rk
         if E < tol:
             running = False
         Rj = Rk
-        print 'Error is '+str(E)
-    return Rk,n
+        return Rk,n
 
 def romberg(f, a, b, tol=1.0e-6):
     '''I, n_panels = romberg(f, a, b, tol=1.0e-6)
@@ -37,5 +32,5 @@ def f(x):
     return math.sin(x)
 
 
-print 'Romberg value is: ' + str(romberg(f,1,10,tol=1e-6))
-print 'Scipy value is: ' + str(rb(f,1,10,tol=1e-6))
+print romberg(f,1,10,tol=1.0**-6)
+print scipy.integrate,quad(f(x),1,10)
