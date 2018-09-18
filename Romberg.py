c77eaf30cf8de73953ebafd48d3b205d7e635cfe
OGMalan <27867110+OGMalan@users.noreply.github.com>
2018-09-18 08:26:51 +0200

    Made changes in a last ditch attempt to get things to work

--- Romberg.py
+++ Romberg.py
@@ -4,42 +4,44 @@ from scipy.integrate import romberg as rb
 
 def Trap(f,a,b,n):
     '''Returns the integral of a function for the range a and b estimated by means of the trapezoidal method using n steps.'''
-    h = (b - a) / float(n)
+    h = (b - a) / float(2**n)
     s = 0.5 * (f(a) + f(b))
     for i in range(1,n):
         s = s + f(a + i*h)
     #print 'Integral of step '+str(n)+' is: '+ str(h*s)
     return h*s
 
-def Richard(k,R,Rk):
-	outlist = []
-	for j in range(k-1,0,-1):
-		if j + 1 == k:
-			output = (4 ** (k - j) * Rk - R[j-1]) / (4 ** (k - j) - 1)
-		else:
-			output = (4 ** (k - j) * R[j] - R[j-1]) / (4 ** (k - j) - 1)
-		outlist.insert(0,output)
-	return outlist
+def Richard(k,R,Rk,f,a,b):
+    outlist = [R[1]]
+    for j in range(k-1,0,-1):
+        if j + 1 == k:
+            output = Trap(f,a,b,k)
+        else:
+            output = (4 ** (k - j) * R[j] - R[j-1]) / (4 ** (k - j) - 1)
+        outlist.insert(0,output)
+    return outlist
 
 def romberg(f, a, b, tol=1.0e-6):
     '''I, n_panels = romberg(f, a, b, tol=1.0e-6)
     Return the integral from a to b of f(x), by Romberg integration,
     as well as the number of panels used'''
     k = 1
-    R = [Trap(f, a, b, 1), Trap(f, a, b, 2)]
+    R = [Trap(f, a, b, 0), Trap(f, a, b, 1)]
     running = True
     while running:
-    	k += 1
-    	Rk = R[0]
-    	R = Richard(k,R,Rk)
-    	E = abs(Rk - R[0])
-    	print 'The error is '+str(E)
-    	if E < tol:
-    		running = False
+        k += 1
+        Rk = R[0]
+        R = Richard(k,R,Rk,f,a,b)
+        print R
+        print Rk
+        E = abs(Rk - R[0])
+        print 'The error is '+str(E)
+        if E < tol:
+            running = False
     return R[0],k
 
 def f(x):
-	return 3*x**2 - 4*x**1 + 5
+	return x**3
 
 start = time.clock()
 my_rom = romberg(f,0,1)
