9b3ef5ec96487284d8958dd9d9baf888173bbe81
OGMalan <27867110+OGMalan@users.noreply.github.com>
2018-09-18 11:17:52 +0200

    It finally works kindof!

--- Romberg.py
+++ Romberg.py
@@ -1,24 +1,22 @@
-import math, time
+import math, time, numpy
 from math import e
 from scipy.integrate import romberg as rb
 
-def Trap(f,a,b,k,Iold):
+def Trap(f,a,b,Iold,k):
     '''Returns the integral of a function for the range a and b estimated by means of the trapezoidal method using k steps.'''
-    H = (b - a) / (2.0)
     if k == 1:
-        Inew = (f(a) + f(b)) / H / 2
-        return Inew
+        Inew = (f(a) + f(b)) * (b - a) / 2.0
     else:
-        n = 2 ** (k - 1)
+        n = 2 ** (k - 2)
         h = (b - a) / n
         x = a + h / 2.0
-        s = 0
+        s = 0.0
         for i in range(n):
             s = s + f(x)
             x = x + h
         Inew = (Iold + h * s) / 2.0
         #print 'Integral of step '+str(n)+' is: '+ str(h*s)
-    return s
+    return Inew
 
 def Richardson(R,k):
     for j in range(k-1,0,-1):
@@ -30,21 +28,27 @@ def romberg(f, a, b, tol=1.0e-6):
     '''I, n_panels = romberg(f, a, b, tol=1.0e-6)
     Return the integral from a to b of f(x), by Romberg integration,
     as well as the number of panels used'''
-    R = []
-    R[1] = Trap(f,a,b,0,1)
+    R = {}
+    R[1] = Trap(f,a,b,0.0,1)
     oldR = R[1]
+    #print 'oldR = '+str(oldR)
     running = True
     k = 1
     while running:
         k += 1
-        R[k] = trapezoid(f,a,b,R[k-1],k)
-        R = richardson(R,k)
-        if abs(R[1] - oldR) < tol:
-            return R[1],2**(k-1)
-        Rold = R[1]
+        #print 'k = '+str(k)
+        R[k] = Trap(f,a,b,R[k-1],k)
+        R = Richardson(R,k)
+        #print 'R1 = ' + str(R[1])
+        #print 'oldR1 = ' + str(oldR)
+        E = abs(R[1] - oldR)
+        #print E
+        if E < tol:
+            return R[1],2**(k-1),k
+        oldR = R[1]
 
 def f(x):
-	return x**3
+	return math.sin(x)
 
 start = time.clock()
 my_rom = romberg(f,0,1)
